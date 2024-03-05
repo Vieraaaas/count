94f61e1fdf6350c9e92ef814626e50f61ab037f6
Claas Paaschburg <paaschburgc@gmail.com>
2024-03-05 14:16:51 +0100

    feat: add progress bar

--- script.js
+++ script.js
@@ -1,15 +1,30 @@
-const counter = document.querySelector("#counter p");
+const counter = document.querySelector("#counter");
 const button = document.querySelector(".reset button");
 let number = 0;
 
-counter.addEventListener("click", function () {
-  number++;
+function updateCount() {
   counter.innerText = number;
-});
+  if (number % 100 !== 0 || number === 0) {
+    counter.style.setProperty("--gradient-width", (number % 100) + "%");
+  } else {
+    counter.style.setProperty("--gradient-width", "100%");
+  }
+}
+
+function increment() {
+  number++;
+  updateCount();
+}
+
+counter.addEventListener("click", increment);
 
 document.addEventListener("keydown", function (event) {
   if (event.key === "Enter" || event.key === " ") {
-    number++;
-    counter.innerText = number;
+    increment();
   }
+});
+
+button.addEventListener("click", function () {
+  number = 0;
+  updateCount();
 });
